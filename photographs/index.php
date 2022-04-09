1c48d39d7d08cfd5f9d6d4e882c024d22b7235d2
Jin <goozenlab@gmail.com>
2022-04-09 09:25:04 +0900

    rebuilding site Sat Apr  9 09:25:04 JST 2022

---
+++ photographs/index.php
@@ -0,0 +1,72 @@
+<?php include ("../../includes/doctype.inc"); ?>
+
+<title>China 6x6 pictures | works | poemograph</title>
+
+<meta name="keywords" content="pinhole, color, experimental, art, japanese, photography" />
+<meta name="description" content="B&W Pictures around Asia using Mamiya 6" />
+
+<script type="text/javascript" language="Javascript" src="/includes/js/base.js"></script>
+<link href="/includes/css/works_index.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+<div id="header">
+<a href="/"><img src="/includes/images/chip/mamiya6.gif" class="chip" /></a>..poemograph.org..<br />
+<a href="/">home</a>&nbsp;&gt;&gt;&nbsp;<a href="/works/">works</a>&nbsp;&gt;&nbsp;index
+</div>
+
+
+<div align="center"> 
+
+
+<h1>index</h1>
+
+<div id="list">
+<table border="0" cellspacing="3" bgcolor="#000000">  
+<tr>
+<td><a href="/works/tc/tnok601.php"><img src="/includes/thumbnails/tnok601.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok604.php"><img src="/includes/thumbnails/tnok604.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok603.php"><img src="/includes/thumbnails/tnok603.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok602.php"><img src="/includes/thumbnails/tnok602.jpg" border="0"></a></td>
+</tr>
+<tr>
+<td><a href="/works/tc/tnok605.php"><img src="/includes/thumbnails/tnok605.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok606.php"><img src="/includes/thumbnails/tnok606.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok607.php"><img src="/includes/thumbnails/tnok607.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok608.php"><img src="/includes/thumbnails/tnok608.jpg" border="0"></a></td>
+</tr>
+<tr>
+<td><a href="/works/tc/tnok611.php"><img src="/includes/thumbnails/tnok611.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok609.php"><img src="/includes/thumbnails/tnok609.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok610.php"><img src="/includes/thumbnails/tnok610.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok612.php"><img src="/includes/thumbnails/tnok612.jpg" border="0"></a></td>
+</tr>
+<tr>
+<td><a href="/works/tc/tnok613.php"><img src="/includes/thumbnails/tnok613.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok614.php"><img src="/includes/thumbnails/tnok614.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok615.php"><img src="/includes/thumbnails/tnok615.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok616.php"><img src="/includes/thumbnails/tnok616.jpg" border="0"></a></td>
+</tr>
+<tr>
+<td><a href="/works/tc/tnok617.php"><img src="/includes/thumbnails/tnok617.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok618.php"><img src="/includes/thumbnails/tnok618.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok619.php"><img src="/includes/thumbnails/tnok619.jpg" border="0"></a></td>
+<td><a href="/works/tc/tnok620.php"><img src="/includes/thumbnails/tnok620.jpg" border="0"></a></td>
+</tr>
+<tr>
+  <td colspan="4"><?php include ("../../includes/ads/gad_46860_black.inc"); ?></td>
+  </tr>
+</table>
+</div>
+<?php /* --- photo ---*/ ?>
+
+
+
+<?php /* --- footer ---*/ ?>
+<?php include ("../../includes/footer.inc"); ?>
+
+</div>
+</body>
+</html>
